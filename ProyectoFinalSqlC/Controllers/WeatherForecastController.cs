fc923a0588ac343e80a533770fc285efbed860e0
nicorm <nicorm2000@gmail.com>
2023-02-12 23:45:40 -0300

    Fixed the path

--- ProyectoFinalSqlC/Controllers/WeatherForecastController.cs
+++ ProyectoFinalSqlC/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace ProyectoFinalSqlc.Controllers
+namespace ProyectoFinalSqlC.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- ProyectoFinalSqlC/Program.cs
+++ ProyectoFinalSqlC/Program.cs
@@ -1,4 +1,4 @@
-namespace ProyectoFinalSqlc
+namespace ProyectoFinalSqlC
 {
     public class Program
     {
