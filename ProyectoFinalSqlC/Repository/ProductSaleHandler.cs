7deaf35378bf55eda7d1892965d811055e943cee
nicorm <nicorm2000@gmail.com>
2023-02-13 21:57:33 -0300

    Updated User and productsale handlers

--- ProyectoFinalSqlC/Repository/ProductSaleHandler.cs
+++ ProyectoFinalSqlC/Repository/ProductSaleHandler.cs
@@ -3,8 +3,41 @@ using ProyectoFinalSqlC.Models;
 
 namespace ProyectoFinalSqlC.Repository
 {
-    public class ProductSaleHandler
+    internal class ProductSaleHandler
     {
+        public static string connectionString = "Data Source=DESKTOP-PKSDVOQ;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
+        public static List<Product> GetSaleProducts(long idUsuario)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                List<long> idProductos = new List<long>();
+
+                SqlCommand comand = new SqlCommand($"SELECT IdProducto FROM Venta INNER JOIN ProductoVendido ON Venta.Id = ProductoVendido.IdVenta WHERE IdUsuario = {idUsuario}", connection);
+                
+                connection.Open();
+
+                SqlDataReader reader = comand.ExecuteReader();
+                
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        idProductos.Add(reader.GetInt64(0));
+                    }
+                }
+                
+                List<Product> productos = new List<Product>();
+
+                foreach (var item in idProductos)
+                {
+                    Product prodTemp = ProductHandler.GetProduct(item);
+
+                    productos.Add(prodTemp);
+                }
+
+                return productos;
+            }
+        }
     }
 }
--- ProyectoFinalSqlC/Repository/UserHandler.cs
+++ ProyectoFinalSqlC/Repository/UserHandler.cs
@@ -3,8 +3,94 @@ using ProyectoFinalSqlC.Models;
 
 namespace ProyectoFinalSqlC.Repository
 {
-    public class UserHandler
+    internal static class UserHandler
     {
+        public static string connectionString = "Data Source=DESKTOP-PKSDVOQ;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public static User GetUser(string username)
+        {
+            User usuario = new User();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand($"SELECT * FROM Usuario Where NombreUsuario = '{username}'", conn);
+
+                conn.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    usuario.Id = reader.GetInt64(0);
+                    usuario.Name = reader.GetString(1);
+                    usuario.Lastname = reader.GetString(2);
+                    usuario.Username = reader.GetString(3);
+                    usuario.Password = reader.GetString(4);
+                    usuario.Email = reader.GetString(5);
+                }
+
+                return usuario;
+            }
+        }
+
+        public static User Login(string username, string password)
+        {
+            User usuario = new User();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand comand = new SqlCommand($"Select * FROM Usuario WHERE NombreUsuario = '{username}' AND Contraseña = '{password}' ", conn);
+                
+                conn.Open();
+                
+                SqlDataReader reader = comand.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    usuario.Id = reader.GetInt64(0);
+                    usuario.Name = reader.GetString(1);
+                    usuario.Lastname = reader.GetString(2);
+                    usuario.Username = reader.GetString(3);
+                    usuario.Password = reader.GetString(4);
+                    usuario.Email = reader.GetString(5);
+                    
+                    return usuario;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static int Register(User usuario)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                
+                SqlCommand comand = new SqlCommand($"INSERT INTO Usuario(Nombre, Apellido, NombreUsuario, Contraseña, Mail)" + $" VALUES('{usuario.Name}','{usuario.Lastname}','{usuario.Username}','{usuario.Password}','{usuario.Email}')", connection);
+
+                return comand.ExecuteNonQuery();
+            }
+        }
+
+        public static int UpdateUser(User usuario)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand($"UPDATE Usuario SET Nombre = '{usuario.Name}', Apellido = '{usuario.Lastname}', NombreUsuario = '{usuario.Username}', Contraseña = '{usuario.Password}', Mail='{usuario.Email}' WHERE Id='{usuario.Id}'", connection);
+
+                return command.ExecuteNonQuery();
+            }
+        }
+
+
+
 
     }
 }
