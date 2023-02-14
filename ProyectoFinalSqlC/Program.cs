54005cda1674df96ba2389353170c5bd712cfc9f
nicorm <nicorm2000@gmail.com>
2023-02-13 22:14:16 -0300

    Updated the main function

--- ProyectoFinalSqlC/Program.cs
+++ ProyectoFinalSqlC/Program.cs
@@ -4,7 +4,34 @@ namespace ProyectoFinalSqlC
     {
         public static void Main(string[] args)
         {
+            var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddControllers();
+
+            builder.Services.AddCors(policy => { policy.AddDefaultPolicy(options => { options.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod(); }); });
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.UseCors();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
         }
     }
 }
